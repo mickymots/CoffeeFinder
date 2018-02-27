1bc726bcb9c52d34c422acab0c21d7bf5b72b1da
Amit Prakash Singh <amit.prakash.singh@accenture.com>
2018-02-27 12:19:38 +0000

    removed old

--- src/app/atm_locator/model/atm-locator.ts
+++
@@ -1,8 +0,0 @@
-export class AtmLocator {
-    constructor(
-        public street: string,
-        public housenumber: string,
-        public postalcode: string,
-        public city: string ) {}
- // tslint:disable-next-line:eofline
- }
