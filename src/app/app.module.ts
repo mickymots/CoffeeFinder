48d901e7e6ad5a379a0ac27c0246c08bb84be111
Amit Prakash Singh <amit.prakash.singh@accenture.com>
2018-02-27 12:26:11 +0000

    1st commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// import {TableModule} from 'primeng/table';
+
+import { AppComponent } from './app.component';
+import { AtmLocatorComponent } from './atm_locator/component/atm-locator.component';
+import { AtmLocatorService } from './atm_locator/service/atm-locator.service';
+import { DataTableModule } from 'primeng/primeng'
+@NgModule({
+  declarations: [AppComponent, AtmLocatorComponent],
+  imports: [BrowserModule, FormsModule, HttpModule, DataTableModule],
+  providers: [AtmLocatorService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/atm_locator/model/atm-locator.ts
@@ -0,0 +1,8 @@
+export class AtmLocator {
+    constructor(
+        public street: string,
+        public housenumber: string,
+        public postalcode: string,
+        public city: string ) {}
+ // tslint:disable-next-line:eofline
+ }
